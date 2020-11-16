c5141ca86a1a929fcb392a5bfe698f93512303d0
Alexandr Tolstov <SESA574650@se.com>
2020-11-16 09:07:05 +0600

    Add project files.

---
+++ SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    public class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        public Person this[int index]
+        {
+            get => (Person)arPeople[index];
+            set => arPeople.Insert(index, value);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return arPeople.GetEnumerator();
+        }
+        public int Count
+        {
+            get => arPeople.Count;
+        }
+    }
+}
