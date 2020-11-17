ec3f874d2b3703c73f4455fb2e0f077bf7d55ded
Alexandr Tolstov <SESA574650@se.com>
2020-11-17 08:02:21 +0600

    1

--- SimpleIndexer/Program.cs
+++ SimpleIndexer/Program.cs
@@ -27,6 +27,24 @@ namespace SimpleIndexer
                 Console.WriteLine("Age: {0}", myPeople[i].Age);
                 Console.WriteLine();
             }
+            Console.WriteLine("____________________________________________");
+            UseGenericListOfPeople();
+        }
+        static void UseGenericListOfPeople()
+        {
+            List<Person> myPeople = new List<Person>();
+            myPeople.Add(new Person("Lisa", "simpson", 2));
+            myPeople.Add(new Person("Bart", "Simpson", 7));
+            // Change first person with indexer.
+            myPeople[0] = new Person("Maggie", "Simpson", 2);
+            // Now obtain and display each item using indexer.
+            for (int i = 0; i < myPeople.Count; i++)
+            {
+                Console.WriteLine("Person number: {0}", i);
+                Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName);
+                Console.WriteLine("Age: {0}", myPeople[i].Age);
+                Console.WriteLine();
+            }
         }
     }
 }
